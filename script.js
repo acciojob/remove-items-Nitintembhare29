44a96cfc8ae017b6b5cf1ecb8883da1055cca588
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-04-18 10:03:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// get the dropdown list and button elements
+  const colorSelect = document.getElementById('colorSelect');
+  const removeButton = document.querySelector('input[type="button"]');
+
+  // add a click event listener to the button
+  removeButton.addEventListener('click', () => {
+    // get the selected option
+    const selectedColor = colorSelect.options[colorSelect.selectedIndex];
+    
+    // remove the selected option
+    selectedColor.remove();
+  });
